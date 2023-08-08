790aa191fa92d6f650479b801f1a1ec7c39caff2
Nikita <81682288+NaViMaTRiX@users.noreply.github.com>
2023-08-08 20:13:29 +0300

    Add title Minutes in Primary

--- src/components/Primary.tsx
+++ src/components/Primary.tsx
@@ -7,10 +7,12 @@ import SettingsContext from "./SettingsContext";
 function Primary() {
 
     const settingsInfo = useContext(SettingsContext);
+    const titleString: string = settingsInfo.mode === "work" ? "Work Minutes" : "Break Minutes"
 
     return (
         <div className="main">
             <p>Flow</p>
+            <p className="mode" style={{color: settingsInfo.mode === "work" ? "yellow" : "green"}}>{titleString}</p>
             <Timer />
             <div className="play">
                 {settingsInfo.isPause
